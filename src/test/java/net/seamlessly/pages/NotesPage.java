c2021fc7407cb6393a4827f8da9ade2b9672ff2a
Arife <arife4231@hotmail.com>
2022-11-13 11:33:23 +0300

    note page commit 2

--- src/test/java/net/seamlessly/pages/NotesPage.java
+++ src/test/java/net/seamlessly/pages/NotesPage.java
@@ -3,16 +3,49 @@ package net.seamlessly.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class NotesPage extends BasePage{
+import java.util.List;
 
-@FindBy(xpath = "//*[@id='appmenu']/li[7]/a")
+public class NotesPage extends BasePage {
+
+    @FindBy(xpath = "//*[@id='appmenu']/li[7]/a")
     public WebElement pencilIcon;
 
-@FindBy(id = "notes_new_note")
+    @FindBy(id = "notes_new_note")
     public WebElement newNoteModule;
 
-   @FindBy(xpath = "//pre[@role='presentation'][1]")
-   public WebElement noteWritingPage;
+    @FindBy(xpath = "//pre[@role='presentation'][1]")
+    public WebElement noteWritingPage;
+
+    @FindBy(xpath = "//ul//li[@class='app-navigation-entry router-link-exact-active active']//button")
+    public WebElement threeDots;
+
+    @FindBy(xpath = "//span[.='Add to favorites']")
+    public WebElement addToFavourites;
+
+    @FindBy(xpath = "//div[@class='app-navigation-entry-icon nav-icon icon-starred']")
+    public WebElement starIcon;
+
+
+    @FindBy(className = "material-design-icon__svg")
+    public WebElement detailsThreeDots;
+
+    @FindBy(xpath = "//button//span[.='Details']")
+    public WebElement detailsButton;
+
+    @FindBy(xpath = "//p[@class='app-sidebar-header__subtitle']")
+    public WebElement countOfWordsCharsText;
+
+    @FindBy(xpath = "//span[@title='Hello World!']")
+    public WebElement note;
+
+    @FindBy(id = "category")
+    public WebElement category;
+
+    @FindBy(xpath = "//input[@class='icon-confirm loading']")
+    public WebElement arrowIcon;
+
+    @FindBy(xpath = "//span[@title='animals']")
+    public WebElement animalCategory;
 
 
 }
--- src/test/java/net/seamlessly/step_definitions/NotesStepDefs.java
+++ src/test/java/net/seamlessly/step_definitions/NotesStepDefs.java
@@ -1,9 +1,11 @@
 package net.seamlessly.step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.seamlessly.pages.NotesPage;
+import net.seamlessly.utilities.BrowserUtils;
 import org.junit.Assert;
 
 public class NotesStepDefs {
@@ -13,11 +15,11 @@ public class NotesStepDefs {
     public void user_click_on_the_pencil_icon_top_of_the_dashboard_page() {
       notesPage.pencilIcon.click();
     }
+
     @When("User clicks on the New Note module")
     public void user_clicks_on_the_new_note_module() {
         notesPage.newNoteModule.click();
     }
-
 
     @Then("User can write notes on the page")
     public void user_can_write_notes_on_the_page() {
@@ -26,7 +28,61 @@ public class NotesStepDefs {
         Assert.assertEquals(notesPage.noteWritingPage.getText(), "Hello World!");
     }
 
+    @When("User clicks three dots near the already written note")
+    public void user_clicks_three_dots_near_the_already_written_note() {
+
+        BrowserUtils.waitFor(2);
+       // BrowserUtils.waitForClickablility(notesPage.threeDots,10);
+         BrowserUtils.clickWithJS((notesPage.threeDots));
+       // notesPage.threeDots.click();
+    }
+
+    @And("User clicks Add to favorites button")
+    public void userClicksButton() {
+        notesPage.addToFavourites.click();
+    }
+
+    @Then("User can add any note to the favorites")
+    public void user_can_add_any_note_to_the_favorites() {
+        Assert.assertTrue(notesPage.starIcon.isDisplayed());
+    }
 
 
+   /* @When("User clicks on the any notes")
+    public void userClicksOnTheAnyNotes() {
+        notesPage.note.click();
+    }
 
+    */
+    
+    
+    @And("User clicks the three dots near the image icon")
+    public void userClicksTheThreeDotsNearTheImageIcon() {
+        notesPage.detailsThreeDots.click();
+    }
+
+    @And("User clicks Details button")
+    public void userClicksDetailsButton() {
+        notesPage.detailsButton.click();
+    }
+
+
+    @Then("User can see the number of letters and words")
+    public void userCanSeeTheNumberOfLettersAndWords() {
+       Assert.assertTrue( notesPage.countOfWordsCharsText.getText().equals("2 words, 12 characters"));
+    }
+
+    @And("User write a category name inside the text box and clicks the arrow button")
+    public void userWriteACategoryNameInsideTheTextBoxAndClicksTheArrowButton() {
+        
+    }
+
+    @And("user clicks Category button on the left side of the page under the New Note module")
+    public void userClicksCategoryButtonOnTheLeftSideOfThePageUnderTheNewNoteModule() {
+        
+    }
+
+    @Then("User can create a new category name")
+    public void userCanCreateANewCategoryName() {
+    }
 }
